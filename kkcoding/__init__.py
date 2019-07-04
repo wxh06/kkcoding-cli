ff23621fada1773fe6860e61eae30bb64f0c4e79
汪心禾 <wangxinhe06@gmail.com>
2019-07-04 23:27:41 +0800

    Update __init__.py
    Save `token` after login and remove when logout.

--- kkcoding/__init__.py
+++ kkcoding/__init__.py
@@ -2,6 +2,8 @@
 
 import getpass
 import json
+import os
+import sys
 
 import requests
 
@@ -11,14 +13,22 @@ def login(username, password):
     return json.loads(requests.post('http://www.kkcoding.net/thrall-web/user/login',
                                     json={'name': username, 'password': password}).text)
 
-def main():
+def main(args):
     "__name == '__main__'"
-    res = login(input('Username: '), getpass.getpass())
-    if not res['data']:
-        raise Exception(res['msg'])
-    user = res['data']
-    token = user['ticket']
-    print(token)
+    confdir = os.path.join(os.path.expanduser('~'), '.kkcoding')
+    os.makedirs(confdir, exist_ok=True)
+    if args[0] == 'login':
+        res = login(input('Username: '), getpass.getpass())
+        if not res['data']:
+            raise Exception(res['msg'])
+        user = res['data']
+        token = user['ticket']
+        with open(os.path.join(confdir, 'token.txt'), 'w') as file:
+            file.write(token)
+    elif args[0] == 'logout':
+        os.remove(os.path.join(confdir, 'token.txt'))
+    else:
+        token = open(os.path.join(confdir, 'token.txt')).read()
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv[1:])
